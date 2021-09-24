1faef476aa9d41e6f4b02652b4a0830e68b8a519
burigo <fernandobt8@gmail.com>
2021-09-24 19:14:36 -0300

    logging

--- frontend/src/monitor/log/Logging.tsx
+++ frontend/src/monitor/log/Logging.tsx
@@ -4,28 +4,40 @@ import styled from 'styled-components'
 import api from '../../api'
 import { FlexBox } from '../../components/FlexBox'
 import { Input } from '../../components/Input'
+import { Label } from '../../components/Label'
 import useHeight from '../../utils/useHeight'
 import { InstanceParams } from '../InstanceMenu'
+
+const MB = 1024 * 1024
 
 export function Logging() {
   const { id } = useParams<InstanceParams>()
   const [log, setLog] = useState<string>()
+  const [totalRange, setTotalRange] = useState<string>('nope')
   const [height, ref] = useHeight({ initialHeight: 0, deps: [] })
   const [minByte, setMinByte] = useState<number>(0)
-  const [maxByte, setMaxByte] = useState<number>(1024)
+  const [maxByte, setMaxByte] = useState<number>(1)
 
   useEffect(() => {
-    api.logFile(id, { Range: `bytes=${minByte * 1024}-${maxByte * 1024}` }).then(response => {
-      setLog(response?.data?.body)
+    api.logFile(id, { Range: `bytes=${Math.ceil(minByte * MB)}-${Math.ceil(maxByte * MB)}` }).then(({ data }) => {
+      setLog(data?.body)
+      const range = data?.headers?.['content-range']
+      const nRange = Number(range?.substring(range?.lastIndexOf('/') + 1))
+      if (!isNaN(nRange)) {
+        setTotalRange((nRange / MB).toFixed(2))
+      }
     })
   }, [id, maxByte, minByte])
 
   return (
     <>
-      <div ref={ref}>
-        <FlexBox>
+      <div ref={ref} style={{ paddingBottom: '10px' }}>
+        <FlexBox gap={10} justifyContent='flex-start'>
+          <Label>Min. MB</Label>
           <Input type='number' value={minByte} onChange={e => setMinByte(Number(e.target.value))} />
+          <Label>Max. MB</Label>
           <Input type='number' value={maxByte} onChange={e => setMaxByte(Number(e.target.value))} />
+          <Label>{`Total ${totalRange}MB`}</Label>
         </FlexBox>
       </div>
       <Log height={(height as number) + 190}>{log}</Log>
